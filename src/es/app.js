c0a381c7cc5fc51a8626cd9c075850ff2b6a7296
Ryo Kawabata <kawabata@kinsha.co.jp>
2020-05-29 22:27:47 +0900

    first commit

---
+++ src/es/app.js
@@ -0,0 +1,9 @@
+class SmallAnimal extends Parent {
+    constructor() {
+        this.animalType = "ポメラニアン";
+    }
+
+    say() {
+        console.log(`${this.animalType}だけどMSの中に永らく居たBOM信者の全身の毛をむしりたい`);
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+
+  mode: 'development',
+
+  entry: './src/es/app.js',
+  output: {
+      path: path.resolve(__dirname, 'www/assets/js'),
+      filename: 'app.min.js'
+  },
+  
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env'
+              ]
+            }
+          }
+        ],
+        exclude: /node_modules/,
+      }
+    ]
+  }
+};
